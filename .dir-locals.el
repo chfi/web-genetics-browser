937a88decb2f0a90f8ab269bdd08f8c4ced64434
Christian Fischer <christian@chfi.se>
2021-07-26 11:58:09 -0500

    add .dir-locals.el & .cargo/config to make rust-analyzer use wasm32

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((lsp-mode . ((lsp-rust-target . "wasm32-unknown-unknown")))
+ (rust-mode . ((lsp-rust-target . wasm32-unknown-unknown))))
