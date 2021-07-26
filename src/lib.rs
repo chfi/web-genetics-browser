28ff2e8b32a951d506837932fc626ae119f75162
Christian Fischer <christian@chfi.se>
2021-07-26 11:25:38 -0500

    test requesting and loading JSON data

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,10 @@ mod view;
 use state::SharedState;
 use wasm_bindgen::prelude::*;
 
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
 use geometry::{Point, Vertex};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -31,6 +35,36 @@ use winit::{
 };
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    // opts.mode(RequestMode::Cors);
+
+    // let url = format!("https://api.github.com/repos/{}/branches/master", repo);
+    let url = format!("http://localhost:8080/gwas.json");
+
+    let request = Request::new_with_str_and_init(&url, &opts).unwrap();
+
+    let web_window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(web_window.fetch_with_request(&request))
+        .await
+        .unwrap();
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json().unwrap()).await.unwrap();
+
+    web_sys::console::log_1(&format!("received json").into());
+    web_sys::console::log_1(&json);
+
+    /*
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
+    */
+
     let size = window.inner_size();
     let instance = wgpu::Instance::new(wgpu::BackendBit::all());
     let surface = unsafe { instance.create_surface(&window) };
